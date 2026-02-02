7b70f94f26b8f289dd868bb5ab3472d7d7a589cf
flo24102007 <erenflo1234@gmail.com>
2026-02-02 15:17:41 +0000

    nouveau

--- Equipe2-Backend/src/mvc_bateau/model/Voyage.java
+++ Equipe2-Backend/src/mvc_bateau/model/Voyage.java
@@ -3,11 +3,146 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package mvc_bateau.model;
+import java.util.Objects;
+import mvc_bateau.infra.model.BaseModel;
+import mvc_bateau.infra.dao.Column; 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 
 /**
  *
  * @author flo
  */
-public class Voyage {
+public class Voyage extends BaseModel {
+    @Column("lieu_depart")
+    private String lieuDepart;
+
+    @Column("lieu_arrive")
+    private String lieuArrive;
+
+    @Column("date_heure_depart")
+    private LocalDateTime  dateHeureDepart;
+
+    @Column("date_heure_arrive")
+    private LocalDateTime dateHeureArrive;
+
+    private Integer prix;
+
+    @Column("siege_reserver")
+    private Integer siegeReserver;
+
+    public Voyage() {
+        this.prix = 0;
+        this.siegeReserver = 0;
+    }
+
+    public Voyage(
+            String lieuDepart,
+            String lieuArrive,
+            LocalDateTime dateHeureDepart,
+            LocalDateTime dateHeureArrive,
+            Integer prix,
+            Integer siegeReserver
+    ) {
+        this.lieuDepart = lieuDepart;
+        this.lieuArrive = lieuArrive;
+        this.dateHeureDepart = dateHeureDepart;
+        this.dateHeureArrive = dateHeureArrive;
+        this.prix = prix;
+        this.siegeReserver = siegeReserver;
+    }
+
+    @Override
+    public Object[] toTableRow() {
+        return new Object[]{
+            id,
+            lieuDepart,
+            lieuArrive,
+            dateHeureDepart,
+            dateHeureArrive,
+            prix,
+            siegeReserver
+        };
+    }
+
+    // ===== Getters & Setters =====
+
+    public String getLieuDepart() {
+        return lieuDepart;
+    }
+
+    public void setLieuDepart(String lieuDepart) {
+        this.lieuDepart = lieuDepart;
+    }
+
+    public String getLieuArrive() {
+        return lieuArrive;
+    }
+
+    public void setLieuArrive(String lieuArrive) {
+        this.lieuArrive = lieuArrive;
+    }
+
+    public LocalDateTime getDateHeureDepart() {
+        return dateHeureDepart;
+    }
+
+    public void setDateHeureDepart(LocalDateTime dateHeureDepart) {
+        this.dateHeureDepart = dateHeureDepart;
+    }
+
+    public LocalDateTime getDateHeureArrive() {
+        return dateHeureArrive;
+    }
+
+    public void setDateHeureArrive(LocalDateTime dateHeureArrive) {
+        this.dateHeureArrive = dateHeureArrive;
+    }
+
+    public Integer getPrix() {
+        return prix;
+    }
+
+    public void setPrix(Integer prix) {
+        this.prix = prix;
+    }
+
+    public Integer getSiegeReserver() {
+        return siegeReserver;
+    }
+
+    public void setSiegeReserver(Integer siegeReserver) {
+        this.siegeReserver = siegeReserver;
+    }
+
+    // ===== equals / hashCode =====
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lieuDepart, lieuArrive, dateHeureDepart);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Voyage other = (Voyage) obj;
+        return Objects.equals(lieuDepart, other.lieuDepart)
+                && Objects.equals(lieuArrive, other.lieuArrive)
+                && Objects.equals(dateHeureDepart, other.dateHeureDepart);
+    }
+
+    @Override
+    public String toString() {
+        return "Voyage{" +
+                "lieuDepart=" + lieuDepart +
+                ", lieuArrive=" + lieuArrive +
+                ", dateHeureDepart=" + dateHeureDepart +
+                ", dateHeureArrive=" + dateHeureArrive +
+                ", prix=" + prix +
+                ", siegeReserver=" + siegeReserver +
+                '}';
+    }
     
 }
